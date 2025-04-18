ce9ecc376160bde21c5348418c5f3b06121b2ae8
NAVEEN KUMAR <nk946432@gmail.com>
2025-04-18 16:29:01 +0530

    Assignment Completed

---
+++ storybook-static/assets/Header.stories-CcFp-hf-.js
@@ -0,0 +1,7 @@
+import{fn as o}from"./index-BgJgh-x_.js";import{H as p}from"./Header-BN27P6uk.js";import"./index-D4lIrffr.js";import"./index-B2-v9W4q.js";import"./Button-D2Yw4dm_.js";const f={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,c,m;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const L=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,L as __namedExportsOrder,f as default};
